a119c7a9da4b80e9e89665dd20a4695ff04b6a30
baneboz <bmutko@gmail.com>
2023-03-21 18:08:13 +0100

    Blog with styles

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -2,14 +2,12 @@ import "./css/App.css";
 
 import BlogPost from "./components/BlogPost";
 import Header from "./components/Header.js";
-import Form from "./components/Form";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <main className="main">
-        <Form />
         <div className="container">
           <BlogPost />
         </div>
